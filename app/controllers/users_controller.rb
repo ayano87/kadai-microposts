1a0fbb98430ce345c8e1e0312becf03afb619d9a
takoyaki <akg_pun87@yahoo.co.jp>
2018-02-16 08:30:30 +0000

    add favorite-bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def favorites
+      @user = current_user
+      @micropost = current_user.microposts.build 
+      @microposts = current_user.feed_microposts.order("created_at DESC").page(params[:page])
+      counts(@user)
+  end
   
   private
   
