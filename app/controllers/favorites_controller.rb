2a7e24ed8113577d221ff89423a434234e707926
takoyaki <akg_pun87@yahoo.co.jp>
2018-02-18 09:03:11 +0000

    add favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    micropost = User.find(params[:micropost_id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
-    flash[:success] = "お気に入り登録しました。"
-    redirect_to root
+    flash[:success] = "お気に入りに登録しました。"
+    redirect_back(fallback_location: root_url)
   end
 
   def destroy
-    micropost = User.find(params[:micropost_id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
-    flash[:success] = "お気に入り登録しました。"
-    redirect_to root
+    flash[:success] = "お気に入りを解除しました。"
+    redirect_back(fallback_location: root_url)
   end
 end
